cacdd8c5cadcf7951bce111c16832abf40e33bb7
Sylvan Smit <sylvan@hey.com>
2024-11-26 22:53:16 +0100

    Connect to database via URL
    Signed-off-by: Sylvan Smit <sylvan@hey.com>

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Tables
 A cli for interacting with SQL databases.
+
+
+# Next Steps
+- [x] Create a test database to test against
+- [x] Create and confirm a database connection
+- [ ] Close connection gracefully
+
+# Resources
+1. [Sample databases](https://github.com/lerocha/chinook-database)
+
+# Testing
+1. Install docker: https://docs.docker.com/engine/install/fedora/
+1. Start database: `./scripts/init_db.sh`
+1. If database already created, just run the migration: `SKIP_DOCKER=true ./scripts/init_db.sh` 
+1. Run the project: `cargo r -- -u postgres://app:secret@localhost:5432/newsletter`
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use sqlx::postgres::PgPoolOptions;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -8,8 +9,28 @@ struct Args {
     url: Option<String>,
 }
 
-fn main() {
+// TODO Change the return sqlx::Error to a tables::Error, which can be returned gracefully to the
+// user.
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
     let args = Args::parse();
 
-    println!("{:?}", args.url);
+    if let Some(url) = args.url {
+        println!("Connection URL: {url}");
+
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&url)
+            .await?;
+
+        println!("Connection successful!");
+
+        let row: (i64,) = sqlx::query_as("SELECT $1")
+            .bind(150_i64)
+            .fetch_one(&pool)
+            .await?;
+
+        assert_eq!(row.0, 150);
+    }
+    Ok(())
 }
