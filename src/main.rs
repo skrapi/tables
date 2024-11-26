d4f0ee1569038369cf24b3b6bf2f5f78e6bb0787
Sylvan Smit <sylvan@hey.com>
2024-11-26 20:03:13 +0100

    Take a URL for the database as an arg
    Signed-off-by: Sylvan Smit <sylvan@hey.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// URL to database connection
+    #[arg(short, long)]
+    url: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args.url);
 }
